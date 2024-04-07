f470dbaf2bc4e98efc8ab811efd98547d619f553
nayacess <104166846+nayacess@users.noreply.github.com>
2024-04-07 14:51:17 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 // Main.java
+import model.DAO;
 
 public class Main {
     public static void main(String[] args) {
-        // Création d'une instance de DatabaseConnector
-        model.DatabaseConnector databaseConnector = new model.DatabaseConnector();
+        // Création d'une instance de DAO
+        DAO dao = new DAO() {
+            // Implémentation des méthodes abstraites si nécessaire
+        };
 
-        // Appel de la méthode connect pour établir la connexion à la base de données
-        databaseConnector.connect();
+        // La connexion à la base de données est établie automatiquement
     }
 }
